d857d041127412bd298711c11a4b689a473da815
Kactuswow <bryanalvarezg.kcts@gmail.com>
2024-04-27 13:09:18 -0500

    gsap overflow errors

--- src/app/[lang]/components/frontendProject/webProjects.tsx
+++ src/app/[lang]/components/frontendProject/webProjects.tsx
@@ -10,7 +10,7 @@ interface Props {
 export const WebProjects = ({ webProjects, lang }: Props): JSX.Element => {
   const { visit } = getLocale(lang).indie
   return (
-    <div className='flex flex-wrap justify-center gap-3 p-2 overflow-x-hidden'>
+    <div className='flex flex-wrap justify-center gap-3 p-2'>
       {
         webProjects.map(webProject => webProject.type !== projectTypes.backend && <WebProject key={webProject.key} webProject={webProject} visitLink={visit} />)
       }
