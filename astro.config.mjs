5ed725b5a3942bf32b6814574ea0365d9fba089d
BryanAG-kcts <bryanalvarezg.kcts@gmail.com>
2025-08-24 10:23:13 -0500

    add:  Sitemap.xml

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,16 @@
 // @ts-check
 import react from '@astrojs/react'
+import sitemap from '@astrojs/sitemap'
 import tailwindcss from '@tailwindcss/vite'
 import { defineConfig } from 'astro/config'
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://portfolio-kcts.vercel.app',
   vite: {
     plugins: [tailwindcss()]
   },
-  integrations: [react()],
+  integrations: [react(), sitemap()],
   i18n: {
     defaultLocale: 'en',
     locales: ['en', 'es'],
